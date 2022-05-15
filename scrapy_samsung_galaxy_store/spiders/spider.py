dd5d99d82796c2ae9e9931a74c0a97efc63a0cfe
minormending <54426030+minormending@users.noreply.github.com>
2022-05-15 14:07:53 -0400

    run black

--- scrapy_samsung_galaxy_store/spiders/spider.py
+++ scrapy_samsung_galaxy_store/spiders/spider.py
@@ -2,9 +2,12 @@ import scrapy
 
 
 class SpiderSpider(scrapy.Spider):
-    name = 'spider'
-    allowed_domains = ['galaxystore.samsung.com']
-    start_urls = ['http://galaxystore.samsung.com/']
+    name = "spider"
+    allowed_domains = ["galaxystore.samsung.com"]
+    start_urls = ["http://galaxystore.samsung.com/"]
+
+    def start_requests(self):
+        return super().start_requests()
 
     def parse(self, response):
         pass
