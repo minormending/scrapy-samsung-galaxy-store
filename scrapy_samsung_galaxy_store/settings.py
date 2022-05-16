b1626e83cfd4423d0b7ef55482b489fc884184c1
minormending <54426030+minormending@users.noreply.github.com>
2022-05-15 21:18:35 -0400

    robots fetch does not work with custom scheme

--- scrapy_samsung_galaxy_store/settings.py
+++ scrapy_samsung_galaxy_store/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "scrapy_samsung_galaxy_store.spiders"
 # USER_AGENT = 'scrapy_samsung_galaxy_store (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 32
