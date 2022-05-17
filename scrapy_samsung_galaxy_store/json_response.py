1b34ff961585ae5c82a5db46976154d8e4f54793
minormending <54426030+minormending@users.noreply.github.com>
2022-05-17 06:53:20 -0400

    simplify JsonResponse

--- scrapy_samsung_galaxy_store/json_response.py
+++ scrapy_samsung_galaxy_store/json_response.py
@@ -1,35 +1,13 @@
 import json
 from typing import Any, Dict, List
-from scrapy import signals, Spider
-from scrapy.http import Request, Response
+from scrapy.http import Response
+
 
 class JsonResponse(Response):
-    def __init__(
-        self,
-        url: str,
-        jobject: List[Any] | Dict[Any, Any],
-        status: int=200,
-        headers: Dict[str, Any]=None,
-        body=b"",
-        flags=None,
-        request=None,
-        certificate=None,
-        ip_address=None,
-        protocol=None,
-    ):
-        super().__init__(
-            url,
-            status,
-            headers,
-            body,
-            flags,
-            request,
-            certificate,
-            ip_address,
-            protocol,
-        )
-        self.jobject = jobject
-        self.jtext = None
+    def __init__(self, jobject: List[Any] | Dict[Any, Any], **kwargs):
+        super().__init__(**kwargs)
+        self.jobject: List[Any] | Dict[Any, Any] = jobject
+        self.jtext: str = None
 
     @property
     def text(self):
